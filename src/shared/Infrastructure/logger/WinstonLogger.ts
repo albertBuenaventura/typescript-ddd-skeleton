360bf7332e453ae12de34f4d722a406185e72208
Albert Buenaventura <albert.buenaventura2009@gmail.com>
2020-02-23 21:22:03 +0800

    winston logger singleton

--- src/shared/Infrastructure/logger/WinstonLogger.ts
+++ src/shared/Infrastructure/logger/WinstonLogger.ts
@@ -2,10 +2,18 @@ import { createLogger, transports, Logger, format } from 'winston'
 import IWinstonLogger from '../../contracts/IWinstonLogger'
 
 export class WinstonLogger implements IWinstonLogger{
+    private static instance: IWinstonLogger;
     private readonly logFile:string = 'all.log'
     private readonly errorLog:string = 'error.log'
     
-    getLogger(): Logger  {
+    static getInstance(): IWinstonLogger {
+      if (!WinstonLogger.instance) {
+        WinstonLogger.instance = new WinstonLogger();
+    }
+    return WinstonLogger.instance;
+    }
+    
+    public getLogger(): Logger  {
       const logger = createLogger({
         level: 'info',
         format: format.json(),
