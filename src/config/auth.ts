c8b260b7efeb3f0593a4dd601b007a5fe2324c03
Albert Buenaventura <albert.buenaventura2009@gmail.com>
2020-02-24 22:31:28 +0800

    Removed the config files

--- src/config/auth.ts
+++
@@ -1,4 +0,0 @@
-export const config = {
-    secret: process.env.APP_SECRET,
-    tokenExpiryTime: 300
-}
--- src/config/server.ts
+++
@@ -1,3 +0,0 @@
-export const isProduction = process.env.server === 'production'
-export const isDev = process.env.server === 'development'
-export const isStaging = process.env.server === 'staging'
